411fbf95574eaedc3692133b9b87b2f5d728a970
adriandcoding <theamr91@gmail.com>
2024-05-20 19:51:00 +0200

    añadiendo funcionalidades

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { changeImage } from "./show card and rotate/main";
+import { changeImage,changeImage2 } from "./show card and rotate/main";
 
 // Añade un evento de clic al botón
 const toggle = document.querySelector(".image-container");
 if (toggle) {
     toggle.addEventListener('click', changeImage);
+}
+const toggle2 = document.querySelector(".image-container2");
+if (toggle2) {
+    toggle2.addEventListener('click', changeImage2);
 }
--- src/show card and rotate/main.ts
+++ src/show card and rotate/main.ts
@@ -20,5 +20,28 @@ export function changeImage(): void {
     }
    
 }
+export function changeImage2(): void {
+    const imgContainer=document.querySelector(".image-container2")
+    if (imgContainer) {
+       // Crea un elemento de imagen
+    const img = document.createElement('img');
+    
+    // Establece la URL de la imagen
+    img.src ="https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/3.png" 
+    
+    // Establece atributos opcionales como el ancho, el alto y el texto alternativo
+    img.width = 150;
+    img.height = 150;
+    img.alt = '';
+
+    // Limpia cualquier contenido previo en el contenedor
+    imgContainer.innerHTML = '';
+    
+    // Añade la imagen al contenedor
+    imgContainer.appendChild(img);
+    }
+   
+}
+
 
 
